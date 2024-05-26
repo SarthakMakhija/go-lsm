f9fcabeb8da87e47ee30d9f066669246574b8783
sarthak <sarthak.makhija@gmail.com>
2024-05-26 18:10:07 +0530

    Sarthak | Adds a batch test

---
+++ txn/batch_test.go
@@ -0,0 +1,19 @@
+package txn
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestBatchWithASingleEntry(t *testing.T) {
+	batch := NewBatch().Put(NewStringKey("consensus"), NewStringValue("raft"))
+	assert.Equal(t, 1, len(batch.AllEntries()))
+}
+
+func TestBatchWithTwoEntries(t *testing.T) {
+	batch := NewBatch().
+		Put(NewStringKey("consensus"), NewStringValue("raft")).
+		Delete(NewStringKey("consensus"))
+
+	assert.Equal(t, 2, len(batch.AllEntries()))
+}
