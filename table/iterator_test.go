2ab509d2d4f46662fa0a3b91f294e8e307eca35f
sarthak <sarthak.makhija@gmail.com>
2024-05-27 19:04:09 +0530

    Sarthak | Adds a test in block iterator for nil value

--- table/iterator_test.go
+++ table/iterator_test.go
@@ -100,6 +100,22 @@ func TestBlockSeekToTheKeyGreaterThanTheSpecifiedKeyFollowedByNext(t *testing.T)
 	assert.False(t, iterator.IsValid())
 }
 
+func TestBlockSeekToTheMatchingKeyWithAnEmptyValue(t *testing.T) {
+	blockBuilder := NewBlockBuilder(4096)
+	blockBuilder.add(txn.NewStringKey("consensus"), txn.EmptyValue)
+
+	block := blockBuilder.build()
+	iterator := SeekToKey(block, txn.NewStringKey("consensus"))
+
+	assert.True(t, iterator.IsValid())
+	assert.Equal(t, txn.NewStringKey("consensus"), iterator.Key())
+	assert.Equal(t, txn.NewStringValue(""), iterator.Value())
+
+	_ = iterator.Next()
+
+	assert.False(t, iterator.IsValid())
+}
+
 func TestBlockSeekToTheNonExistingKey(t *testing.T) {
 	blockBuilder := NewBlockBuilder(4096)
 	blockBuilder.add(txn.NewStringKey("consensus"), txn.NewStringValue("raft"))
