2520902e61ee773d41b22ca2bbea95f84e10ebd9
sarthak <sarthak.makhija@gmail.com>
2024-07-31 13:48:49 +0530

    Sarthak | Gets rid of NewStringKey from table_bloom_filter_test

--- table/table_bloom_filter_test.go
+++ table/table_bloom_filter_test.go
@@ -10,9 +10,9 @@ import (
 
 func TestLoadSSTableWithSingleBlockAndCheckKeysForExistenceUsingBloom(t *testing.T) {
 	ssTableBuilder := NewSSTableBuilder(4096)
-	ssTableBuilder.Add(txn.NewStringKey("consensus"), txn.NewStringValue("raft"))
-	ssTableBuilder.Add(txn.NewStringKey("distributed"), txn.NewStringValue("TiKV"))
-	ssTableBuilder.Add(txn.NewStringKey("etcd"), txn.NewStringValue("bbolt"))
+	ssTableBuilder.Add(txn.NewStringKeyWithTimestamp("consensus", 5), txn.NewStringValue("raft"))
+	ssTableBuilder.Add(txn.NewStringKeyWithTimestamp("distributed", 6), txn.NewStringValue("TiKV"))
+	ssTableBuilder.Add(txn.NewStringKeyWithTimestamp("etcd", 7), txn.NewStringValue("bbolt"))
 
 	tempDirectory := os.TempDir()
 	filePath := filepath.Join(tempDirectory, "TestLoadSSTableWithSingleBlockAndCheckKeysForExistenceUsingBloom.log")
@@ -23,16 +23,16 @@ func TestLoadSSTableWithSingleBlockAndCheckKeysForExistenceUsingBloom(t *testing
 	ssTable, err := Load(1, filePath, 4096)
 
 	assert.Nil(t, err)
-	assert.True(t, ssTable.MayContain(txn.NewStringKey("consensus")))
-	assert.True(t, ssTable.MayContain(txn.NewStringKey("distributed")))
-	assert.True(t, ssTable.MayContain(txn.NewStringKey("etcd")))
+	assert.True(t, ssTable.MayContain(txn.NewStringKeyWithTimestamp("consensus", 8)))
+	assert.True(t, ssTable.MayContain(txn.NewStringKeyWithTimestamp("distributed", 9)))
+	assert.True(t, ssTable.MayContain(txn.NewStringKeyWithTimestamp("etcd", 10)))
 }
 
 func TestLoadSSTableWithSingleBlockAndCheckKeysForNonExistenceUsingBloom(t *testing.T) {
 	ssTableBuilder := NewSSTableBuilder(4096)
-	ssTableBuilder.Add(txn.NewStringKey("consensus"), txn.NewStringValue("raft"))
-	ssTableBuilder.Add(txn.NewStringKey("distributed"), txn.NewStringValue("TiKV"))
-	ssTableBuilder.Add(txn.NewStringKey("etcd"), txn.NewStringValue("bbolt"))
+	ssTableBuilder.Add(txn.NewStringKeyWithTimestamp("consensus", 5), txn.NewStringValue("raft"))
+	ssTableBuilder.Add(txn.NewStringKeyWithTimestamp("distributed", 6), txn.NewStringValue("TiKV"))
+	ssTableBuilder.Add(txn.NewStringKeyWithTimestamp("etcd", 6), txn.NewStringValue("bbolt"))
 
 	tempDirectory := os.TempDir()
 	filePath := filepath.Join(tempDirectory, "TestLoadSSTableWithSingleBlockAndCheckKeysForNonExistenceUsingBloom.log")
@@ -43,14 +43,14 @@ func TestLoadSSTableWithSingleBlockAndCheckKeysForNonExistenceUsingBloom(t *test
 	ssTable, err := Load(1, filePath, 4096)
 
 	assert.Nil(t, err)
-	assert.False(t, ssTable.MayContain(txn.NewStringKey("paxos")))
-	assert.False(t, ssTable.MayContain(txn.NewStringKey("bolt")))
+	assert.False(t, ssTable.MayContain(txn.NewStringKeyWithTimestamp("paxos", 7)))
+	assert.False(t, ssTable.MayContain(txn.NewStringKeyWithTimestamp("bolt", 7)))
 }
 
 func TestLoadAnSSTableWithTwoBlocksAndCheckKeysForExistenceUsingBloom(t *testing.T) {
 	ssTableBuilder := NewSSTableBuilder(30)
-	ssTableBuilder.Add(txn.NewStringKey("consensus"), txn.NewStringValue("raft"))
-	ssTableBuilder.Add(txn.NewStringKey("distributed"), txn.NewStringValue("TiKV"))
+	ssTableBuilder.Add(txn.NewStringKeyWithTimestamp("consensus", 5), txn.NewStringValue("raft"))
+	ssTableBuilder.Add(txn.NewStringKeyWithTimestamp("distributed", 6), txn.NewStringValue("TiKV"))
 
 	tempDirectory := os.TempDir()
 	filePath := filepath.Join(tempDirectory, "TestLoadAnSSTableWithTwoBlocksAndCheckKeysForExistenceUsingBloom.log")
@@ -60,7 +60,7 @@ func TestLoadAnSSTableWithTwoBlocksAndCheckKeysForExistenceUsingBloom(t *testing
 
 	ssTable, err := Load(1, filePath, 30)
 	assert.Nil(t, err)
-	assert.True(t, ssTable.MayContain(txn.NewStringKey("consensus")))
-	assert.True(t, ssTable.MayContain(txn.NewStringKey("distributed")))
-	assert.False(t, ssTable.MayContain(txn.NewStringKey("etcd")))
+	assert.True(t, ssTable.MayContain(txn.NewStringKeyWithTimestamp("consensus", 7)))
+	assert.True(t, ssTable.MayContain(txn.NewStringKeyWithTimestamp("distributed", 7)))
+	assert.False(t, ssTable.MayContain(txn.NewStringKeyWithTimestamp("etcd", 8)))
 }
