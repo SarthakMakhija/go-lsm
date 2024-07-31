c0017634776568e528bfea5733bdcaab76bcafcd
sarthak <sarthak.makhija@gmail.com>
2024-07-31 18:32:46 +0530

    Sarthak | Gets rid of NewStringKey from indexed_iterator_test

--- iterator/indexed_iterator_test.go
+++ iterator/indexed_iterator_test.go
@@ -8,11 +8,11 @@ import (
 
 func TestAnIndexedIteratorBasedOnKey(t *testing.T) {
 	indexedIteratorOne := NewIndexedIterator(0, newTestIteratorNoEndKey(
-		[]txn.Key{txn.NewStringKey("consensus")},
+		[]txn.Key{txn.NewStringKeyWithTimestamp("consensus", 10)},
 		[]txn.Value{txn.NewStringValue("raft")},
 	))
 	indexedIteratorOther := NewIndexedIterator(1, newTestIteratorNoEndKey(
-		[]txn.Key{txn.NewStringKey("distributed")},
+		[]txn.Key{txn.NewStringKeyWithTimestamp("distributed", 2)},
 		[]txn.Value{txn.NewStringValue("db")},
 	))
 
@@ -21,13 +21,26 @@ func TestAnIndexedIteratorBasedOnKey(t *testing.T) {
 
 func TestAnIndexedIteratorBasedOnSameKeyWithDifferentIteratorIndex(t *testing.T) {
 	indexedIteratorOne := NewIndexedIterator(0, newTestIteratorNoEndKey(
-		[]txn.Key{txn.NewStringKey("consensus")},
+		[]txn.Key{txn.NewStringKeyWithTimestamp("consensus", 5)},
 		[]txn.Value{txn.NewStringValue("raft")},
 	))
 	indexedIteratorOther := NewIndexedIterator(1, newTestIteratorNoEndKey(
-		[]txn.Key{txn.NewStringKey("consensus")},
+		[]txn.Key{txn.NewStringKeyWithTimestamp("consensus", 5)},
 		[]txn.Value{txn.NewStringValue("db")},
 	))
 
 	assert.True(t, indexedIteratorOne.IsPrioritizedOver(indexedIteratorOther))
+}
+
+func TestAnIndexedIteratorBasedOnSameKeyWithDifferentTimestamp(t *testing.T) {
+	indexedIteratorOne := NewIndexedIterator(0, newTestIteratorNoEndKey(
+		[]txn.Key{txn.NewStringKeyWithTimestamp("consensus", 5)},
+		[]txn.Value{txn.NewStringValue("raft")},
+	))
+	indexedIteratorOther := NewIndexedIterator(1, newTestIteratorNoEndKey(
+		[]txn.Key{txn.NewStringKeyWithTimestamp("consensus", 6)},
+		[]txn.Value{txn.NewStringValue("db")},
+	))
+
+	assert.True(t, indexedIteratorOther.IsPrioritizedOver(indexedIteratorOne))
 }
