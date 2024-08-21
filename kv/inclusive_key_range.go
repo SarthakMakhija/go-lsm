b35e4ecd93911bf86c174d2f9758fe14e9404f54
sarthak <sarthak.makhija@gmail.com>
2024-08-21 20:22:32 +0530

    Sarthak | Adds documentation for InclusiveKeyRange

--- kv/inclusive_key_range.go
+++ kv/inclusive_key_range.go
@@ -1,14 +1,19 @@
 package kv
 
+// LessOrEqual defines a contract for any key to be a part of InclusiveKeyRange.
+// Currently, Key and RawKey implement LessOrEqual.
 type LessOrEqual interface {
 	IsLessThanOrEqualTo(other LessOrEqual) bool
 }
 
+// InclusiveKeyRange represents a key range with an inclusive end key.
+// To keep things simple, the implementation only supports inclusive range in scan or scan-like operations.
 type InclusiveKeyRange[T LessOrEqual] struct {
 	start T
 	end   T
 }
 
+// NewInclusiveKeyRange creates a new instance of InclusiveKeyRange if start is less than or equal to the end, panics otherwise.
 func NewInclusiveKeyRange[T LessOrEqual](start, end T) InclusiveKeyRange[T] {
 	if start.IsLessThanOrEqualTo(end) {
 		return InclusiveKeyRange[T]{
@@ -19,10 +24,12 @@ func NewInclusiveKeyRange[T LessOrEqual](start, end T) InclusiveKeyRange[T] {
 	panic("end key must be greater than or equal to start key in InclusiveKeyRange")
 }
 
+// Start returns the start key.
 func (inclusiveKeyRange InclusiveKeyRange[T]) Start() T {
 	return inclusiveKeyRange.start
 }
 
+// End returns the end key.
 func (inclusiveKeyRange InclusiveKeyRange[T]) End() T {
 	return inclusiveKeyRange.end
 }
