93334b34815210ae3d5601390e965fa058d3a621
sarthak <sarthak.makhija@gmail.com>
2024-08-24 13:48:13 +0530

    Sarthak | Adds the documentation for Transaction

--- txn/transaction.go
+++ txn/transaction.go
@@ -28,6 +28,12 @@ A brief background on serialized-snapshot-isolation:
 More details are available [here](https://tech-lessons.in/en/blog/serializable_snapshot_isolation/).
 */
 
+// Transaction represents both Readwrite and Readonly transactions.
+// Every transaction gets a begin-timestamp and has an instance of Oracle and state.StorageState.
+// An instance of Readwrite transaction maintains:
+// - a reference to kv.Batch which is a collection of key/value pairs, that a transaction operates on.
+// - a collection of all the keys read within the transaction.
+// readLock is used as a lock over the `reads` field, because multiple iterators can be created in a Readwrite transaction.
 type Transaction struct {
 	oracle         *Oracle
 	state          *state.StorageState
@@ -38,6 +44,7 @@ type Transaction struct {
 	readLock       sync.Mutex
 }
 
+// NewReadonlyTransaction creates a new instance of Readonly transaction.
 func NewReadonlyTransaction(oracle *Oracle, state *state.StorageState) *Transaction {
 	return &Transaction{
 		oracle:         oracle,
@@ -49,6 +56,7 @@ func NewReadonlyTransaction(oracle *Oracle, state *state.StorageState) *Transact
 	}
 }
 
+// NewReadwriteTransaction creates a new instance of Readwrite transaction.
 func NewReadwriteTransaction(oracle *Oracle, state *state.StorageState) *Transaction {
 	return &Transaction{
 		oracle:         oracle,
@@ -60,6 +68,12 @@ func NewReadwriteTransaction(oracle *Oracle, state *state.StorageState) *Transac
 	}
 }
 
+// Get gets the value for the given key.
+// It returns a tuple (kv.Value, true), if the key exists, else (kv.EmptyValue, false).
+// The Get method involves the following:
+// 1) Getting the begin-timestamp of the transaction.
+// 2) Getting the value corresponding to the timestamped key from state.StorageState.
+// Please note: the system returns the value where the timestamp of the key in the system <= begin-timestamp of the transaction.
 func (transaction *Transaction) Get(key []byte) (kv.Value, bool) {
 	versionedKey := kv.NewKey(key, transaction.beginTimestamp)
 	if transaction.readonly {
@@ -72,6 +86,12 @@ func (transaction *Transaction) Get(key []byte) (kv.Value, bool) {
 	return transaction.state.Get(versionedKey)
 }
 
+// Scan supports scan operation by taking an instance of kv.InclusiveKeyRange.
+// Scan involves the following:
+// 1) Getting the begin-timestamp of the transaction.
+// 2) Creating a versionedKeyRange.
+// 3) Scanning over state.StorageState if the transaction is a Readonly transaction.
+// 4) Scanning over the kv.Batch and state.StorageState if the transaction is a Readwrite transaction.
 func (transaction *Transaction) Scan(keyRange kv.InclusiveKeyRange[kv.RawKey]) (iterator.Iterator, error) {
 	versionedKeyRange := kv.NewInclusiveKeyRange(
 		kv.NewKey(keyRange.Start(), transaction.beginTimestamp),
@@ -93,6 +113,8 @@ func (transaction *Transaction) Scan(keyRange kv.InclusiveKeyRange[kv.RawKey]) (
 	return transactionIterator, nil
 }
 
+// Set sets the key/value pair in the kv.Batch associated with the Transaction.
+// It panics if the same key is added again or the transaction is a Readonly transaction.
 func (transaction *Transaction) Set(key, value []byte) error {
 	if transaction.readonly {
 		panic("transaction is readonly")
@@ -100,6 +122,8 @@ func (transaction *Transaction) Set(key, value []byte) error {
 	return transaction.batch.Put(key, value)
 }
 
+// Delete adds the key in the kv.Batch.
+// It panics if the transaction is a Readonly transaction.
 func (transaction *Transaction) Delete(key []byte) error {
 	if transaction.readonly {
 		panic("transaction is readonly")
@@ -108,6 +132,13 @@ func (transaction *Transaction) Delete(key []byte) error {
 	return nil
 }
 
+// Commit commits the transaction. It panics if the transaction is Readonly or kv.Batch is empty.
+// Commit involves the following:
+// 1) Acquiring an executorLock to ensure that the transaction are sent to the TransactionExecutor in the order they invoke Commit.
+// 2) Getting the commit timestamp for the transaction. Commit timestamp is only provided if the transaction does not have any RW conflict.
+// 3) Submitting the kv.TimestampedBatch to the Executor.
+// 4) Passing a commit callback to the kv.TimestampedBatch which is invoked when the entire batch is applied.
+// 5) The commit callback informs the `commitTimestampMark` of Oracle that a transaction with `commitTimestamp` is done.
 func (transaction *Transaction) Commit() (*Future, error) {
 	if transaction.readonly {
 		panic("transaction is readonly")
@@ -129,6 +160,7 @@ func (transaction *Transaction) Commit() (*Future, error) {
 	return transaction.oracle.executor.submit(kv.NewTimestampedBatchFrom(*transaction.batch, commitTimestamp), commitCallback), nil
 }
 
+// trackReads keeps a track of all the keys read in the Readwrite transaction.
 func (transaction *Transaction) trackReads(key kv.RawKey) {
 	transaction.readLock.Lock()
 	transaction.reads = append(transaction.reads, key)
