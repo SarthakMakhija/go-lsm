be50adcfc482d1f8493101558146f9b4ce54e193
sarthak <sarthak.makhija@gmail.com>
2024-08-26 13:36:36 +0530

    Sarthak | Adds the documentation for Transaction iterator

--- txn/iterator.go
+++ txn/iterator.go
@@ -5,11 +5,19 @@ import (
 	"go-lsm/kv"
 )
 
+// Iterator represents a readwrite transaction iterator.
+// It holds an instance of iterator.MergeIterator which is created from:
+// - PendingWritesIterator, and
+// - iterator from state.StorageState
+// The main reasons for creating this iterator include:
+// 1) Skipping the deleted keys
+// 2) Tracking reads for a readwrite transaction.
 type Iterator struct {
 	transaction *Transaction
 	inner       *iterator.MergeIterator
 }
 
+// NewTransactionIterator creates a new instance of Iterator for transaction.
 func NewTransactionIterator(transaction *Transaction, inner *iterator.MergeIterator) (*Iterator, error) {
 	transactionIterator := &Iterator{transaction: transaction, inner: inner}
 	if err := transactionIterator.ignoreDeleted(); err != nil {
@@ -21,14 +29,20 @@ func NewTransactionIterator(transaction *Transaction, inner *iterator.MergeItera
 	return transactionIterator, nil
 }
 
+// Key returns the kv.Key.
 func (iterator *Iterator) Key() kv.Key {
 	return iterator.inner.Key()
 }
 
+// Value returns the kv.Value.
 func (iterator *Iterator) Value() kv.Value {
 	return iterator.inner.Value()
 }
 
+// Next involves the following:
+// 1) Moves the merge iterator forward.
+// 2) Ignores deleted keys.
+// 3) Tracks key reads.
 func (iterator *Iterator) Next() error {
 	if err := iterator.inner.Next(); err != nil {
 		return err
@@ -42,14 +56,17 @@ func (iterator *Iterator) Next() error {
 	return nil
 }
 
+// IsValid returns true if the iterator is valid.
 func (iterator *Iterator) IsValid() bool {
 	return iterator.inner.IsValid()
 }
 
+// Close closes the iterator.
 func (iterator *Iterator) Close() {
 	iterator.inner.Close()
 }
 
+// ignoreDeleted keeps moving the MergeIterator forward till the iterator is valid and the key is deleted.
 func (iterator *Iterator) ignoreDeleted() error {
 	for iterator.IsValid() && iterator.Value().IsEmpty() {
 		if err := iterator.inner.Next(); err != nil {
