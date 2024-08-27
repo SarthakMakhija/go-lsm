e037d5aa2a2117cac5e85495123adba184467124
sarthak <sarthak.makhija@gmail.com>
2024-08-27 16:54:58 +0530

    Sarthak | Adds the documentation for SSTableBuilder

--- table/builder.go
+++ table/builder.go
@@ -8,6 +8,7 @@ import (
 	"go-lsm/table/bloom"
 )
 
+// SSTableBuilder allows building SSTable in a step-by-step manner.
 type SSTableBuilder struct {
 	blockBuilder       *block.Builder
 	blockMetaList      *block.MetaList
@@ -18,10 +19,13 @@ type SSTableBuilder struct {
 	blockSize          uint
 }
 
+// NewSSTableBuilderWithDefaultBlockSize creates a new instance of SSTableBuilder with block.DefaultBlockSize = 4Kb.
 func NewSSTableBuilderWithDefaultBlockSize() *SSTableBuilder {
 	return NewSSTableBuilder(block.DefaultBlockSize)
 }
 
+// NewSSTableBuilder creates a new instance of SSTableBuilder with the given block size.
+// The specified block size will be used to limit the size of each block that will be a part of the final SSTable.
 func NewSSTableBuilder(blockSize uint) *SSTableBuilder {
 	return &SSTableBuilder{
 		blockBuilder:       block.NewBlockBuilder(blockSize),
@@ -31,6 +35,12 @@ func NewSSTableBuilder(blockSize uint) *SSTableBuilder {
 	}
 }
 
+// Add adds the key/value pair in the current block builder.
+// Add involves:
+// 1) Keeping a track of the starting key and ending key of the current block.
+// 2) Adding the key to the bloom.FilterBuilder
+// 3) Adding the key/value pair to the current block.Builder.
+// 4) Finishing the current block, if it is full and starting a new block (or block.Builder).
 func (builder *SSTableBuilder) Add(key kv.Key, value kv.Value) {
 	if builder.startingKey.IsRawKeyEmpty() {
 		builder.startingKey = key
@@ -41,12 +51,20 @@ func (builder *SSTableBuilder) Add(key kv.Key, value kv.Value) {
 		return
 	}
 	builder.finishBlock()
-	builder.startNewBlock(key)
+	builder.startNewBlockBuilder(key)
 	builder.blockBuilder.Add(key, value)
 }
 
-// Build
-// TODO: Bloom
+// Build builds the SSTable using the given id and file path.
+// It involves encoding the SSTable, writing the entire table to persistent storage and creating an in-memory representation
+// in the form of SSTable with a reference to its File.
+// The encoding looks like:
+/**
+  ----------------------------------------------------------------------------------------------------------------------------------------------------------
+| data block | data block |...| data block | metadata section | 4 bytes for meta starting offset | bloom filter section | 4 bytes for bloom starting offset |
+|										   |				  |									 |					                                        |
+ ----------------------------------------------------------------------------------------------------------------------------------------------------------
+*/
 func (builder *SSTableBuilder) Build(id uint64, filePath string) (SSTable, error) {
 	blockMetaBeginOffset := func() []byte {
 		blockMetaBeginOffset := make([]byte, block.Uint32Size)
@@ -61,9 +79,9 @@ func (builder *SSTableBuilder) Build(id uint64, filePath string) (SSTable, error
 
 	builder.finishBlock()
 	buffer := new(bytes.Buffer)
-	buffer.Write(builder.allBlocksData)
-	buffer.Write(builder.blockMetaList.Encode())
-	buffer.Write(blockMetaBeginOffset())
+	buffer.Write(builder.allBlocksData)          //data blocks
+	buffer.Write(builder.blockMetaList.Encode()) //metadata section block.MetaList.Encode()
+	buffer.Write(blockMetaBeginOffset())         //4 bytes to indicate where the meta section starts from
 	filter := builder.bloomFilterBuilder.Build(bloom.FalsePositiveRate)
 	encodedFilter, err := filter.Encode()
 	if err != nil {
@@ -71,14 +89,13 @@ func (builder *SSTableBuilder) Build(id uint64, filePath string) (SSTable, error
 	}
 
 	bloomFilterOffset := bloomOffset(buffer)
-	buffer.Write(encodedFilter)
-	buffer.Write(bloomFilterOffset)
+	buffer.Write(encodedFilter)     //bloom filter section bloom.Filter.Encode()
+	buffer.Write(bloomFilterOffset) //4 bytes to indicate where the bloom filter section starts from
 
 	file, err := Create(filePath, buffer.Bytes())
 	if err != nil {
 		return SSTable{}, err
 	}
-	//TODO: Block cache + bloom fields
 
 	startingKey, _ := builder.blockMetaList.StartingKeyOfFirstBlock()
 	endingKey, _ := builder.blockMetaList.EndingKeyOfLastBlock()
@@ -94,10 +111,15 @@ func (builder *SSTableBuilder) Build(id uint64, filePath string) (SSTable, error
 	}, nil
 }
 
+// EstimatedSize returns an estimate of the size of the encoded data of all the blocks.
 func (builder SSTableBuilder) EstimatedSize() int {
 	return len(builder.allBlocksData)
 }
 
+// finishBlock finishes the current block. It involves:
+// 1) Encoding the current block.
+// 2) Storing the block.Meta in the block meta-list.
+// 3) Collecting the encoded data of the current block in allBlocksData.
 func (builder *SSTableBuilder) finishBlock() {
 	encodedBlock := builder.blockBuilder.Build().Encode()
 	builder.blockMetaList.Add(block.Meta{
@@ -108,7 +130,8 @@ func (builder *SSTableBuilder) finishBlock() {
 	builder.allBlocksData = append(builder.allBlocksData, encodedBlock...)
 }
 
-func (builder *SSTableBuilder) startNewBlock(key kv.Key) {
+// startNewBlockBuilder creates a new instance of SSTableBuilder.
+func (builder *SSTableBuilder) startNewBlockBuilder(key kv.Key) {
 	builder.blockBuilder = block.NewBlockBuilder(builder.blockSize)
 	builder.startingKey = key
 	builder.endingKey = key
