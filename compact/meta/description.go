a57a8261f50d72b6551a8724dc2c7068f59b5377
sarthak <sarthak.makhija@gmail.com>
2024-09-22 16:51:05 +0530

    Sarthak | Adds the documentation for StorageStateChangeEvent

--- compact/meta/description.go
+++ compact/meta/description.go
@@ -4,6 +4,8 @@ package meta
 var NothingToCompactDescription = SimpleLeveledCompactionDescription{}
 
 // SimpleLeveledCompactionDescription defines the table.SSTable ids between adjacent levels which will undergo compaction.
+// Between level0 and level1, level0 would be UpperLevel, level1 would be LowerLevel.
+// Similarly, between level1 and level2, level1 would be UpperLevel, level2 would be LowerLevel.
 type SimpleLeveledCompactionDescription struct {
 	UpperLevel           int
 	LowerLevel           int
--- state/state_change_event.go
+++ state/state_change_event.go
@@ -9,6 +9,8 @@ import (
 
 var NoStorageStateChanges = StorageStateChangeEvent{anyChanges: false}
 
+// StorageStateChangeEvent represents a state change event for StorageState.
+// It is generated after compaction runs, and it compacts table.SSTable files from adjacent levels.
 type StorageStateChangeEvent struct {
 	NewSSTables   []*table.SSTable
 	NewSSTableIds []uint64
@@ -16,6 +18,7 @@ type StorageStateChangeEvent struct {
 	anyChanges    bool
 }
 
+// NewStorageStateChangeEvent creates a new instance of StorageStateChangeEvent.
 func NewStorageStateChangeEvent(newSSTables []*table.SSTable, description meta.SimpleLeveledCompactionDescription) StorageStateChangeEvent {
 	newSSTableIds := make([]uint64, 0, len(newSSTables))
 	for _, ssTable := range newSSTables {
@@ -29,6 +32,7 @@ func NewStorageStateChangeEvent(newSSTables []*table.SSTable, description meta.S
 	}
 }
 
+// NewStorageStateChangeEventByOpeningSSTables creates a new instance of StorageStateChangeEvent, by opening the newSSTableIds.
 func NewStorageStateChangeEventByOpeningSSTables(newSSTableIds []uint64, description meta.SimpleLeveledCompactionDescription, rootPath string) (StorageStateChangeEvent, error) {
 	newSSTables := make([]*table.SSTable, 0, len(newSSTableIds))
 	for _, ssTableId := range newSSTableIds {
@@ -46,34 +50,43 @@ func NewStorageStateChangeEventByOpeningSSTables(newSSTableIds []uint64, descrip
 	}, nil
 }
 
+// CompactionUpperLevel returns the upper level present in meta.SimpleLeveledCompactionDescription.
 func (event StorageStateChangeEvent) CompactionUpperLevel() int {
 	return event.description.UpperLevel
 }
 
+// CompactionLowerLevel returns the lower level present in meta.SimpleLeveledCompactionDescription.
 func (event StorageStateChangeEvent) CompactionLowerLevel() int {
 	return event.description.LowerLevel
 }
 
+// CompactionUpperLevelSSTableIds returns the SSTableIds present in upper level of meta.SimpleLeveledCompactionDescription.
 func (event StorageStateChangeEvent) CompactionUpperLevelSSTableIds() []uint64 {
 	return event.description.UpperLevelSSTableIds
 }
 
+// CompactionLowerLevelSSTableIds returns the SSTableIds present in lower level of meta.SimpleLeveledCompactionDescription.
 func (event StorageStateChangeEvent) CompactionLowerLevelSSTableIds() []uint64 {
 	return event.description.LowerLevelSSTableIds
 }
 
+// CompactionDescription returns the instance of meta.SimpleLeveledCompactionDescription.
 func (event StorageStateChangeEvent) CompactionDescription() meta.SimpleLeveledCompactionDescription {
 	return event.description
 }
 
+// MaxSSTableId returns the max SSTableId from NewSSTableIds.
 func (event StorageStateChangeEvent) MaxSSTableId() uint64 {
 	return slices.Max(event.NewSSTableIds)
 }
 
+// HasAnyChanges returns true if StorageStateChangeEvent has any changes, meaning if the compaction ran between two levels.
 func (event StorageStateChangeEvent) HasAnyChanges() bool {
 	return event.anyChanges
 }
 
+// allSSTableIdsExcludingTheOnesPresentInUpperLevelSSTableIds returns all the SSTableIds from the upper level, excluding the
+// given SSTableIds.
 func (event StorageStateChangeEvent) allSSTableIdsExcludingTheOnesPresentInUpperLevelSSTableIds(ssTableIds []uint64) []uint64 {
 	var excludedSSTableIds []uint64
 
@@ -86,6 +99,7 @@ func (event StorageStateChangeEvent) allSSTableIdsExcludingTheOnesPresentInUpper
 	return excludedSSTableIds
 }
 
+// upperLevelSSTableIdsAsMap returns the SSTableIds present in upper level as a map.
 func (event StorageStateChangeEvent) upperLevelSSTableIdsAsMap() map[uint64]struct{} {
 	ssTableIds := make(map[uint64]struct{}, len(event.CompactionUpperLevelSSTableIds()))
 	for _, ssTableId := range event.CompactionUpperLevelSSTableIds() {
