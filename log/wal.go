69b7d165c2071f20ad28f85c34f64053813c56f4
sarthak <sarthak.makhija@gmail.com>
2024-09-23 00:35:35 +0530

    Sarthak | Updates the documentation of WAL, corrects a typo

--- log/wal.go
+++ log/wal.go
@@ -29,7 +29,7 @@ func NewWALForId(id uint64, walDirectoryPath string) (*WAL, error) {
 // Recovery involves the following:
 // 1) Opening the file in READONLY & APPEND mode.
 // 2) Reading the whole file.
-// 3) Iterating through the file buffer (/bytes) and decoding each the bytes to get kv.Key and kv.Value.
+// 3) Iterating through the file buffer (/bytes) and decoding the bytes to get kv.Key and kv.Value.
 // 4) Invoking the provided callback with kv.Key and kv.Value.
 // There are a few approaches in terms of reading the WAL:
 //  1. Read the whole file.
